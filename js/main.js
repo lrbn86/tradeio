a769372b41541c4f11a86aea0d328414c16dcfca
lrbn86 <lrbn86@gmail.com>
2022-05-21 04:04:38 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ let buyingPower = 400000;
 const portfolioValueDisplay = document.querySelector('#portfolio-value');
 const buyingPowerDisplay = document.querySelector('#buying-power');
 const watchlistContainer = document.querySelector('#watchlist');
-const stockInfoContainer = document.querySelector('#stock-info');
+const displayContainer = document.querySelector('#display-container');
+const stockInfoContainer = document.querySelector('#stock-info-container');
 
 stockInfoContainer.style.display = "none";
 
@@ -15,35 +16,36 @@ document.addEventListener('click', (event) => {
   const buyBtn = event.target.closest('#buy-btn');
   const sellBtn = event.target.closest('#sell-btn');
 
-  let stockInfo;
-
   if (watchListItem) {
-    stockInfo = {
-      "stockName": watchListItem.childNodes[1].childNodes[1].textContent,
-      "stockPrice": watchListItem.childNodes[3].childNodes[1].textContent,
-      "stockPriceDeltaPercentage": watchListItem.childNodes[3].childNodes[3].textContent,
-      "stockShares": watchListItem.childNodes[1].childNodes[3].textContent
+    const stockObj = {
+      "stockName": watchListItem.querySelector('.stock-name').textContent,
+      "stockPrice": watchListItem.querySelector('.stock-price').textContent,
+      "stockPriceDeltaPercentage": watchListItem.querySelector('.stock-percent').textContent,
+      "stockShares": watchListItem.querySelector('.stock-shares').textContent
     };
-    stockInfoContainer.childNodes[3].textContent = stockInfo.stockName;
-    stockInfoContainer.childNodes[5].textContent = stockInfo.stockPrice;
-    stockInfoContainer.childNodes[7].textContent = stockInfo.stockPriceDeltaPercentage;
-    stockInfoContainer.childNodes[9].textContent = stockInfo.stockShares;
+
+    document.querySelector('#stock-name').textContent = stockObj.stockName;
+    document.querySelector('#stock-price').textContent = stockObj.stockPrice;
+    document.querySelector('#stock-percent').textContent = stockObj.stockPriceDeltaPercentage;
+    document.querySelector('#stock-shares').textContent = stockObj.stockShares;
+
     stockInfoContainer.style.display = "block";
     watchlistContainer.style.display = "none";
 
   }
+
   if (backBtn) {
     watchlistContainer.style.display = "block";
     stockInfoContainer.style.display = "none";
   }
 
   if (buyBtn) {
-    console.log('BUY', stockInfo);
-
+    let stockPrice = document.querySelector('#stock-price');
+    portfolioValueDisplay.textContent = stockPrice.textContent;
   }
 
   if (sellBtn) {
-    console.log('SELL', stockInfo);
+    let stockPrice = document.querySelector('#stock-price');
   }
 
 
