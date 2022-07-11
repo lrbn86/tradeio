892a7df517e6f0daec348c7c7aa059a9c01d7d8b
lrbn86 <lrbn86@gmail.com>
2022-07-10 20:09:49 -0400

    Create mock data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    symbol: 'AMZN',
+    shares: 25,
+    totalReturn: 210
+  },
+  {
+    symbol: 'MSFT',
+    shares: 35,
+    totalReturn: 150
+  },
+  {
+    symbol: 'AAPL',
+    shares: 95,
+    totalReturn: -50
+  },
+]
+
+export const watchlist = [
+  {
+    symbol: 'AMZN',
+    currentPrice: 114
+  },
+  {
+    symbol: 'MSFT',
+    currentPrice: 240
+  },
+  {
+    symbol: 'AAPL',
+    currentPrice: 150
+  },
+]
